e06593f9a8fb0b1bb63b2c9db22481e9c9628945
JannatulFerdusShima <98678178+JannatulFerdusShima@users.noreply.github.com>
2022-02-07 11:17:59 +0600

    Add files via upload

---
+++ Task1/Task.php
@@ -0,0 +1,68 @@
+<html>
+    <head>
+</head>
+
+<body>
+
+<table>
+    <h2>Registration Forms</h2>
+    <From action="/action_page.php">
+        
+            <tr>
+       <td>Frist Name:</td>
+                  <td><input type="text" name="fname"></td>
+    </tr>
+       <tr>
+        <td>Last Name:</td>
+          <td><input type ="text" name="fname"></td>
+    </tr>
+       <td> Age:</td>
+              <td><input type ="text" name="fname"></td>
+      </tr>
+     <tr>
+      <td> Designation:</td>
+       <td> <input type ="radio" name ="Des">Junior Programmer
+        <input type ="radio" name ="Des">Senior Programmer
+        <input type ="radio" name ="Des">Project Lead
+    </td>
+
+     </tr>
+      
+
+     <tr>
+      <td>Preferred Language:</td>
+       <td> <input type ="checkbox" name ="PL">Java
+        <input type ="checkbox" name ="PL">PHP
+        <input type ="checkbox" name ="PL">C++
+
+</td>
+    </tr>
+
+     <tr>
+       <td> E-mail:</td>
+        <td><input type="text" name="ename"></td>
+</tr>
+      <tr>
+        <td>Password:</td>
+        <td><input type ="Password" value="fakePSW"></td>
+</tr>
+     <tr>
+        <td>Please choose a file</td>
+       <td> <input type="file" name="myfile"></td>
+</tr>
+
+<tr>
+
+<td><input type="submit" value="Submit">
+    <intput type="reset" value="Reset">
+</td>
+</tr>
+
+ </table>
+   
+</form>
+      </body>
+
+    </html>
+
+        
